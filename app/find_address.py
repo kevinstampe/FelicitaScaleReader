f46c2547219a1c1dadf1d3c4204ff180e7a8e47b
Kevin Stampe <kst@vertica.dk>
2024-11-13 23:57:45 +0100

    find address fix

--- app/find_address.py
+++ app/find_address.py
@@ -3,29 +3,29 @@ from bleak import BleakScanner
 
 from const import SCALE_START_NAMES
 
-
-async def find_acaia_devices(timeout=10, scanner: BleakScanner | None = None) -> list:
-    if scanner is None:
-        async with BleakScanner() as scanner:
-            return await scan(scanner, timeout)
-    else:
-        return await scan(scanner, timeout)
-    
-
-async def scan(scanner: BleakScanner, timeout) -> list:
+async def find_acaia_devices():
     addresses = []
+    await asyncio.sleep(1)
+    scanner = BleakScanner()
+    try:
+        devices = await scanner.discover(timeout=5.0)
+        for d in devices:
+            if d.name and any(d.name.startswith(name) for name in SCALE_START_NAMES):
+                print(d.name, d.address)
+                addresses.append(d.address)
+        return addresses
+    except Exception as e:
+        if e.error_name == "org.bluez.Error.InProgress":
+            print("Another scan is already in progress, retrying...")
+            await asyncio.sleep(1)  # Retry delay
+            return await find_acaia_devices()
+        else:
+            print(f"Unexpected Bluetooth error: {e}")
 
-    devices = await scanner.discover(timeout=timeout)
-    for d in devices:
-        if d.name and any(d.name.startswith(name) for name in SCALE_START_NAMES):
-            print(d.name, d.address)
-            addresses.append(d.address)
+felicitaAddresses = asyncio.run(find_acaia_devices())
+
 
     # write to mac_addresses.txt
-    with open("mac_addresses.txt", "w") as f:
-        for address in addresses:
-            f.write(f"{address}\n")
-
-    return addresses
-
-asyncio.run(find_acaia_devices())
+with open("mac_addresses.txt", "w") as f:
+    for address in felicitaAddresses:
+        f.write(f"{address}\n")
