814758895f9b890511fd8c9d7dfcbb41135702e2
Kevin Stampe <kst@vertica.dk>
2024-11-14 20:25:33 +0100

    maybe fix

--- app/platform_check.py
+++ app/platform_check.py
@@ -1,18 +1,18 @@
-def is_raspberry_pi():
-    try:
-        # Check /etc/os-release for "Raspberry Pi" mention
-        with open("/etc/os-release", "r") as f:
-            if "Raspbian" in f.read() or "Raspberry Pi" in f.read():
-                return True
-    except FileNotFoundError:
-        pass
+import os
 
+def is_raspberry_pi():
+    if "rpi" in str(os.uname()).lower():
+        return True
+    
     try:
         # Check if processor is BCM (common for Raspberry Pi models)
         with open("/proc/cpuinfo", "r") as f:
             for line in f:
-                if "BCM" in line:
+                if "raspberry pi" in line.lower():
                     return True
+                if "rpi" in line.lower():
+                    return True
+                
     except FileNotFoundError:
         pass
 
